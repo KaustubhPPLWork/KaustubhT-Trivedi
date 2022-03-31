c67b2007459d0d0147435a5ff63a5252a1be7ab8
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-03-31 18:40:46 +0530

    Learnt Typescript

---
+++ Typescript/index.js
@@ -0,0 +1,11 @@
+var printLog = function () {
+    console.log("This is a TypeScript project");
+};
+printLog();
+var add = function () {
+    var a = 5;
+    var b = 6;
+    var c = a + b;
+    console.log(c);
+};
+add();
