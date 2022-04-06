8300b260e8ef29927a54e21b830859a9581f66e2
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-04-06 18:28:09 +0530

    Counter Code

--- React/Stopwatch/src/App.jsx
+++ React/Stopwatch/src/App.jsx
@@ -2,70 +2,95 @@ import { Fragment, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState({
+    miliseconds: 0,
+    seconds: 0,
+    mins: 0,
+  });
+  // const [mins,setMins] = useState(0)
+  // const [inter, setInter] = useState();
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
   const [count, setCount] = useState(0);
-  const [miliseconds, setMiliseconds] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [status, setStatus] = useState(0);
 
-  /* 0 = Not started
-   1 = started
-   2 = stopped
-  */
-
+  const runClock = () => {
+    // if (isActive && isPaused === false)
+  };
+  // console.log(timer.miliseconds);
+  const updateCounter = () => {
+    setCount((count) => count + 1);
+  };
   const startClock = () => {
-    setStatus(1);
-    setInterval(run, 10);
-    console.log("Clock Started");
+    // startClockTicks;
+    setIsActive(true);
+    setIsPaused(false);
+    setInterval(() => {
+      if (isActive && isPaused === false) {
+        setTimer({
+          ...timer,
+          miliseconds: timer.miliseconds + 10,
+          // miliseconds:
+        });
+        if (timer.miliseconds === 1000) {
+          setTimer({
+            ...timer,
+            miliseconds: 0,
+            seconds: timer.seconds + 1,
+          });
+          if (timer.seconds === 60) {
+            setTimer({
+              ...timer,
+              seconds: 0,
+              mins: timer.mins + 1,
+            });
+          }
+        }
+      }
+
+      // updateCounter();
+    }, 10);
+    console.log("started");
+    console.log(timer.miliseconds);
   };
 
-  const run = () => {
-    setMiliseconds(miliseconds + 1);
-    if (miliseconds === 1000) {
-      setSeconds(seconds + 1);
-      setMiliseconds(0);
-      console.log("seconds updated");
-    }
-    if (seconds === 60) {
-      setMinutes(minutes + 1);
-      setSeconds(0);
-      console.log("minutes Updated");
-    }
-    if (minutes === 60) {
-      setHours(hours + 1);
-      setMinutes(0);
-      console.log("Hours Updated");
-    }
-    console.log(status);
+  const stopClock = () => {
+    // clearInterval(interval);
+    setIsActive(false);
+    console.log("Stopped");
   };
 
   const pauseClock = () => {
-    clearInterval(run);
-    setStatus(2);
+    setIsActive(false);
+    setIsPaused(true);
   };
-
   const resetClock = () => {
-    setHours(0);
-    setMinutes(0);
-    setSeconds(0);
-    setMiliseconds(0);
-    setStatus(0);
+    setIsPaused(false);
+    setIsActive(false);
+    setTimer({
+      miliseconds: 0,
+      seconds: 0,
+      mins: 0,
+    });
   };
-
   return (
     <Fragment>
       <div className="App">
-        <h1>Stopwatch</h1>
-        <p>
-          {hours}:{minutes}:{seconds}:{miliseconds}
-        </p>
+        <h1>
+          {timer.mins}:{timer.seconds}:{timer.miliseconds}
+        </h1>
 
-        {/* <Display time={time} /> */}
         <div className="buttons">
           <button onClick={startClock}>Start</button>
+          <button onClick={stopClock}>Stop</button>
           <button onClick={pauseClock}>Pause</button>
           <button onClick={resetClock}>Reset</button>
+        </div>
+
+        <div>
+          {count}
+
+          <button>Add</button>
+          <button>Subtract</button>
         </div>
       </div>
     </Fragment>
